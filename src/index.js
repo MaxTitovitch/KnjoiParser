813a8d4ff7155642f78da94ea952a55a46e0230c
MaxTitovitch <maxtitovitch@mail.ru>
2020-09-04 19:02:03 +0300

    Cron added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 let {Saver} = require("./services/Saver");
 
-let saver = new Saver();
-saver.startWork();
+let CronJob = require('cron').CronJob;
+let job = new CronJob('0 */20 * * * *', function() {
+  let saver = new Saver();
+  saver.startWork();
+});
+job.start();
