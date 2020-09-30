b2651b0d71a01a1dca1b53fc83436258312f321e
MaxTitovitch <maxtitovitch@mail.ru>
2020-09-30 22:44:58 +0300

    Init

--- src/services/KnjoiParser.js
+++ src/services/KnjoiParser.js
@@ -1,7 +1,20 @@
 
 let puppeteer = require('puppeteer')
 let fs = require('fs')
+const winston = require('winston')
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ],
+});
+
+logger.add(new winston.transports.Console({
+  format: winston.format.simple(),
+}));
 
 module.exports = class KnjoiParser {
   constructor (brandsLink, parentNode, connection) {
